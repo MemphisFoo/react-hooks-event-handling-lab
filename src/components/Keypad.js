6d72598151699a7501527198a6d5a0a13fb1524c
Christian TDN <cjwtdn@gmail.com>
2022-07-19 06:39:51 -0600

    commit tree 6a678d320558ea6c9a16f8d938f4f5026b1f7b3b

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+    function handleKeypad(event) {
+        event.preventDefault(event);
+        console.log("Entering password...");
+    }
+    return (
+        <div>
+            <input type="password" name="password" onChange={handleKeypad} placeholder="Enter password" />
+            <button>Login</button>
+        </div>
+    )
+}
+export default Keypad;
